d8999d5c543e2ca922a5e2feca62f24e8e89ea61
STIVVVV <stevencanfield2000@gmail.com>
2022-05-14 13:14:16 -0400

    Initial components added

---
+++ src/components/Introduction.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Introduction()
+{
+    return (
+        <div>
+            <h1>
+                {/* Add logos to footer */}
+                Carl
+            </h1>
+        </div>
+    )
+}
