ff1e9bc474dc756ba30cc90d736a15241c53b420
STIVVVV <stevencanfield2000@gmail.com>
2022-05-15 23:17:28 -0400

    added style and clickable buttons
    -added links to my git and itch.io

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,17 +1,25 @@
 import React from "react";
+import "../index.css"
 import logo from "../images/logo192.png";
+import gitLogo from "../images/GitHub Icon.png";
+import itchLogo from "../images/itch.io.png";
 
 export default function Introduction()
 {
     return (
         <div>
                 {/*change image*/}
-                <img src={logo} className="logo"/>
+                <img src={logo} className="logo" alt="react logo"/>
                 <h1>name</h1>
                 <h2>Profession</h2>
 
-                <button>github link</button>
-                <button>itch.io link</button>
+                <a href="https://github.com/STIVVVV">
+                    <img className="image" src={gitLogo}  alt="github logo"/>  
+                </a> 
+
+                <a href="https://stivvvv.itch.io/">
+                    <img className="image" src={itchLogo}  alt="itch.io logo"/> 
+                </a> 
         </div>
     )
 }
